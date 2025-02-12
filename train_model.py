717ffdd8e40de9c4e89a9d5fab47dea219f5ce26
Ben Lawton <blawton@Mac.attlocal.net>
2025-02-11 21:50:53 -0800

    Many changes to this model including the incorporation of time of day as a cosine kernel, fixing the lengthscales of period kernels to the length of a day or a year, and the optimization of kernel combinations.

--- train_model.py
+++ train_model.py
@@ -36,15 +36,15 @@ folds=5
 
 #These are for SVGP:
 # Number of inducing locations
-M=500
+M=1000
 #Batch_size for each epoch
 batch_size = 32
 # minibatch size for estimating ELBO
 # minibatch_size=100
-epochs_per_fold=100
-logging_epoch_freq=100
+epochs_per_fold=40
+logging_epoch_freq=10
 # This next condition enforces 1 graph/fold post training
-graphing_epoch_freq=epochs_per_fold
+graphing_epoch_freq=40
 # maximum iterations
 maxiter=10000
 
@@ -53,17 +53,17 @@ indep_var=["Day", "Overall_Day", "Latitude", "Longitude", "embay_dist"]
 area="ES"
 method="SVGP"
 spatial_pca= False
-notes="This version uses SVGP based on GPFlow documentation, with a fixed sin kernel added (uses Adam not LFBGS)"
-model_name="3.6"
+notes="Same as 3.10 but now used on data with outliers removed and k folds shuffled."
+model_name="3.11"
 within_year_vars = np.setdiff1d(np.arange(len(indep_var)), np.array(overall_time_dim))
 # Characteristic Map
 characteristic_names = ['Kernel', 'Optimizer', 'Fold', 'Year', 'Independent Vars', 'Area', 'Method', 'Spatial PCA', 'Model Name', 'Notes', 'Inducing Locations', 'Epochs/Fold', 'Inducing Variables']
 
 #Date
-date = "12_22_2024"
+date = "12_26_2024"
 
 #Filename
-filename="agg_daily_morning_coastal_features.csv"
+filename="agg_daily_morning_w_time_coastal_features_v2.csv"
 
 #
 
@@ -76,19 +76,21 @@ df=pd.read_csv(path, index_col=0)
 df["Continuous"]= False
 df.loc[df["Organization"].isin(["EPA_FISM", "STS_Tier_II", "USGS_Cont"]), "Continuous"]=True
 
-#Adding in Year Var
+#Adding in Year and Day Vars
 df["Date"]=pd.to_datetime(df["Date"])
+df["Day"]=df["Date"].dt.day_of_year
 df["Year"]=df["Date"].dt.year
 
 # Adding in Overall_Day in addition to Day (new in v3.1)
 df["Overall_Day"]= 365*(df["Year"]-min(years)) + df["Day"]
 
 
-#Fixing index and dropping Unnamed: 0
+#Fixing index, dropping Unnamed: 0, and ensuring all dtypes are float
 df.drop("Unnamed: 0", axis=1, inplace=True)
 df.reset_index(inplace=True, drop=True)
+df[indep_var]=df[indep_var].astype(float)
 
-print(df.loc[df["Year"]==2023, :])
+# print(df.loc[df["Year"]==2023, :])
 
 #
 
@@ -109,17 +111,17 @@ def get_kernels():
 
 ## Kernels using within year vars
 
-  matern32_1 = gpflow.kernels.Matern32(lengthscales=[1]*len(within_year_vars),
-                                     active_dims=within_year_vars)
-  matern32_2 = gpflow.kernels.Matern32(lengthscales=[1]*len(within_year_vars),
-                                     active_dims=within_year_vars)
-  matern32_3 = gpflow.kernels.Matern32(lengthscales=[1]*len(within_year_vars),
-                                     active_dims=within_year_vars)
+  # matern32_1 = gpflow.kernels.Matern32(lengthscales=[1]*len(within_year_vars),
+  #                                    active_dims=within_year_vars)
+  # matern32_2 = gpflow.kernels.Matern32(lengthscales=[1]*len(within_year_vars),
+  #                                    active_dims=within_year_vars)
+  # matern32_3 = gpflow.kernels.Matern32(lengthscales=[1]*len(within_year_vars),
+  #                                    active_dims=within_year_vars)
   matern32_4 = gpflow.kernels.Matern32(lengthscales=[1]*len(within_year_vars),
                                      active_dims=within_year_vars)
 
-  rbf_1= gpflow.kernels.RBF(lengthscales=[1]*len(within_year_vars),
-                          active_dims=within_year_vars)
+  # rbf_1= gpflow.kernels.RBF(lengthscales=[1]*len(within_year_vars),
+  #                         active_dims=within_year_vars)
   rbf_2= gpflow.kernels.RBF(lengthscales=[1]*len(within_year_vars),
                           active_dims=within_year_vars)
 
@@ -134,16 +136,16 @@ def get_kernels():
                         active_dims=overall_time_dim)
 
 ## Kernels using cyclical day of year
-  sin_1 = gpflow.kernels.Cosine(lengthscales=[365], variance=.5, active_dims=time_dim)
-  # sin_2 = gpflow.kernels.Cosine(lengthscales=[365], active_dims=time_dim)
+  # sin_1 = gpflow.kernels.Cosine(lengthscales=[365], variance=.5, active_dims=time_dim)
+  sin_2 = gpflow.kernels.Cosine(lengthscales=[365], active_dims=time_dim)
 
   #Testing set (all have an rbf for time within year), note: sin must be last!
   kernels={
-      # "rbf": rbf_1,
-      # "matern32_rbf_intra": matern32_1 + rbf_time_1,
-      "matern32_rbf_intra_sin": matern32_2 + rbf_time_2 + sin_1
+      # "rbf": rbf_1
+      # "matern32_rbf_intra": matern32_1 + rbf_time_1
+      # "matern32_rbf_intra_sin": matern32_2 + rbf_time_2 + sin_1
       # "rbf_matern32_plus": rbf_1 + matern32_3 + rbf_time_3
-      # "rbf_matern32_sin_plus": rbf_2 + matern32_4 + sin_2 + rbf_time_4
+      "rbf_rbf_matern32_sin": rbf_2 + matern32_4 + rbf_time_4 + sin_2
 
       # "rbf_rq": rbf_3 + rq_1,
       # "matern32_rq": matern32_3 +rq_2
@@ -189,7 +191,14 @@ print(standardize(np.arange(1,10), np.arange(1,10)*10))
 
 #
 
-#Building model in each fold
+# Test sample for getting ELBO estimate (temporary)
+
+train_sample=df.sample(1000)
+Xtrain_d = train_sample[indep_var].values
+ytrain_d = train_sample[dep_var].values
+data = (Xtrain_d, ytrain_d)
+tensor_data = tuple(map(tf.convert_to_tensor, data))
+
 #Building model in each fold
 def CV_fold(Xtrain, Xtest, ytrain, ytest,
             train_types, test_types,
@@ -198,7 +207,9 @@ def CV_fold(Xtrain, Xtest, ytrain, ytest,
   agg_models={}
   agg_params=pd.DataFrame()
   agg_rmse=pd.DataFrame()
+  running_rmse = pd.DataFrame()
   optimizers=get_opt()
+
   for o_name, optimizer in optimizers.items():
 
     #Kernels should be reset before usage of each optimizer
@@ -228,8 +239,8 @@ def CV_fold(Xtrain, Xtest, ytrain, ytest,
       # Setting whether or not parameters are trainable
 
       ## Cyclical Sin
-      # if k_name[-3:]=="sin":
-      #   gpflow.utilities.set_trainable(m.kernel.kernels[-1].lengthscales, False)
+      if k_name[-3:]=="sin":
+        gpflow.utilities.set_trainable(m.kernel.kernels[-1].lengthscales, False)
       #   gpflow.utilities.set_trainable(m.kernel.kernels[-1].variance, False)
 
       ## Inducing variables
@@ -275,12 +286,12 @@ def CV_fold(Xtrain, Xtest, ytrain, ytest,
 
             epoch_id = epoch + 1
             if epoch_id % logging_epoch_freq == 0:
-                # tf.print(f"Epoch {epoch_id}: ELBO (train) {model.elbo(data)}")
-                tf.print(epoch)
+                tf.print(f"Epoch {epoch_id}: ELBO (train) {model.elbo(tensor_data)}")
+                # tf.print(epoch)
 
       # Training and monitoring loop
       monitor_cycles = max((epochs_per_fold // graphing_epoch_freq), 1)
-
+      running_epoch = 0
       for i in range(monitor_cycles):
 
         ## Actually running iteration loop
@@ -288,9 +299,13 @@ def CV_fold(Xtrain, Xtest, ytrain, ytest,
                             logging_epoch_freq=logging_epoch_freq,
                             iter_training_data=iter(train_dataset)
                             )
+
+        running_epoch+=graphing_epoch_freq
+
         ## Predicting for monitoring
         pred_mean, pred_var = m.predict_y(Xtest_s)
-        actual_data = ytest_s
+
+        ## Graphing
         fig, ax = plt.subplots()
         ax.set_title(k_name)
         ax.scatter(Xtest_s[:, overall_time_dim], pred_mean, c = 'Red', alpha=.15)
@@ -300,6 +315,48 @@ def CV_fold(Xtrain, Xtest, ytrain, ytest,
                                                str(fold),
                                                str(year),
                                                ]) + ".png")
+
+        ## Running rmse
+        working = pd.DataFrame(columns=["Code", "RMSE", "Wghted RMSE", "Cont RMSE",
+                                        "Discrete RMSE", "y_std", "Epoch"], index=[0])
+
+        working.loc[0, "Code"] = model_name + "_" + "_".join([k_name,
+                                               o_name,
+                                               str(fold),
+                                               str(year),
+                                               ])
+
+        working.loc[0, "Epoch"] = running_epoch
+        working.loc[0, "RMSE"] = np.sqrt(np.mean((pred_mean - ytest_s)[:, 0] ** 2))
+
+        # For location-weighted RMSE, we assume stratified sample of stations,
+        # 1st group is continuous stations, 2nd group is discrete statons
+        # Actual population frequency should be 50-50, see derivation in notes on
+        # creating an unbiased predictor
+        if np.sum(test_types) > 0 and np.sum(np.logical_not(test_types)) > 0:
+            # Weighted RMSE
+            working.loc[0, "Wghted RMSE"] = np.sqrt(
+                .5 * np.nanmean(np.where(test_types, (pred_mean - ytest_s)[:, 0], np.nan) ** 2)
+                + .5 * np.nanmean(np.where(test_types == False, (pred_mean - ytest_s)[:, 0], np.nan) ** 2)
+            )
+        if np.sum(test_types) > 0:
+            # For RMSE of continuous stations only
+            working.loc[0, "Cont RMSE"] = np.sqrt(
+                np.nanmean(np.where(test_types, (pred_mean - ytest_s)[:, 0], np.nan) ** 2)
+            )
+        if np.sum(np.logical_not(test_types)) > 0:
+            # For RMSE of discrete stations only
+            working.loc[0, "Discrete RMSE"] = np.sqrt(
+                np.nanmean(np.where(test_types == False, (pred_mean - ytest_s)[:, 0], np.nan) ** 2)
+            )
+
+        # Std dev of y
+        working.loc[0, "y_std"] = np.std(ytest)
+
+        # # Training Time
+        # working["Training Time"] = training_time
+
+        running_rmse = pd.concat([running_rmse, working])
 
       #Storing model for later recreation
       param_dict=gpflow.utilities.parameter_dict(m)
@@ -365,13 +422,13 @@ def CV_fold(Xtrain, Xtest, ytrain, ytest,
       working["Training Time"] = training_time
 
       #For live monitoring
-      print(working["RMSE"])
+      print(running_rmse)
 
       #Setting characteristics and saving
       working[characteristic_names] = pd.DataFrame([characteristics], index=working.index)
       agg_rmse=pd.concat([agg_rmse, working])
 
-  return(agg_models, agg_params, agg_rmse)
+  return(agg_models, agg_params, agg_rmse, running_rmse)
 
 #
 
@@ -402,8 +459,8 @@ def CV_fold(Xtrain, Xtest, ytrain, ytest,
 
 #
 
-# Running model in all folds (with small test data to start -statement below picks random samples)
-df = df.sample(30)
+# # Running model in all folds (with small test data to start -statement below picks random samples)
+# df = df.sample(50)
 
 ## Dummy (will be removed later)
 year = 0
@@ -412,43 +469,63 @@ year = 0
 agg_models = {}
 agg_params = pd.DataFrame()
 agg_rmse = pd.DataFrame()
+agg_running_rmse = pd.DataFrame()
 
+## Getting the folds for training
 fold_data = get_folds(df, year, folds, dep_var, indep_var, group_vars)
 
+## Optional storing of folds for reference
+# save_output = pd.DataFrame()
+# for i in range(folds):
+#     working = fold_data[i]
+#     working_Xtrain = working[0]
+#     working_ytrain = working[2]
+#
+#     working_df = pd.DataFrame(working_Xtrain, columns = [indep_var])
+#     working_df['ytrain'] = working_ytrain
+#     working_df["fold"] = i
+#     save_output = pd.concat([save_output, working_df])
+#
+# print("Test of save output:")
+# print(save_output)
+# save_output.to_csv("train_folds_through_3.10.csv")
+
+## Running model
 assert(folds == len(fold_data))
 
 for i in range(folds):
 
-    models, params, rmse = CV_fold(*fold_data[i])
+    models, params, rmse, running_rmse = CV_fold(*fold_data[i])
 
     agg_models.update(models)
     agg_params = pd.concat([agg_params, params])
     agg_rmse = pd.concat([agg_rmse, rmse])
+    agg_running_rmse = pd.concat([agg_running_rmse, running_rmse])
 
     # Progress
     print("Fold", i, "complete")
 
-
-#Save all results
+# Saving all results
 
 model_path="models_" + model_name + "_" + date +".csv"
 param_path="params_" + model_name +  "_" + date + ".csv"
 rmse_path="rmse_" + model_name + "_" + date +".csv"
+running_rmse_path="running_rmse_" + model_name + "_" + date +".csv"
 
-#Save results
+## Saving model performance and details
 agg_models=pd.DataFrame(agg_models)
 agg_models.to_csv(model_path)
 agg_params.to_csv(param_path)
 agg_rmse.to_csv(rmse_path)
+agg_running_rmse.to_csv(running_rmse_path)
 
-#Getting inducing variables parameter
+## Getting inducing variables parameter
 assert(sum(list(agg_models.index.str[:18]==".inducing_variable"))==1)
 ind_index = list(agg_models.index.str[:18]==".inducing_variable").index(True)
 print(ind_index)
 
 
-#Saving inducing variables specifically
-# print(agg_models.values)
+## Saving inducing variables specifically
 manual_output = agg_models.values[:][ind_index].copy()
 index = agg_models.columns.values
 
@@ -462,4 +539,6 @@ for i in range(len(index)):
 
   output.to_csv(model_name + "_inducing_variables_" + name + ".csv")
 
-print(agg_rmse.loc[agg_rmse["Kernel"]=="matern32_rbf_intra"].groupby(["Year", "Fold"])[["RMSE", "Training Time"]].mean())
+## Printing quick summary of results
+print(agg_rmse.loc[agg_rmse["Kernel"]=="matern32_rbf_sin_intra"].groupby(["Year", "Fold"])[["RMSE", "Training Time"]].mean())
+print(agg_running_rmse)
